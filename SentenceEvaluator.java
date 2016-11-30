21e571310bf2a4f3589d9fed571d59720e5b5fcc
Marius <mlatinis@gmail.com>
2016-11-30 15:50:53 +0000

    Added null check for scoring function

--- SentenceEvaluator.java
+++ SentenceEvaluator.java
@@ -4,10 +4,13 @@ import java.util.Map;
 public class SentenceEvaluator {
 
 	int Score(ArrayList<String> sentence,
-			Map<String, Integer> wordFrequencyCount) {
+			Map<String, Integer> wordsFrequencies) {
 		int score = 0;
 		for (String word : sentence) {
-			score += wordFrequencyCount.get(word);
+			Integer wordFrequency = wordsFrequencies.get(word);
+			if (wordFrequency != null) {
+				score += wordFrequency;
+			}
 		}
 		return score;
 
